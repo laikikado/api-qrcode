3d67ba782d506a043ec9dfb8f5b85208dfb1de4f
alexandrecros1 <alexandre.cros1@epsi.fr>
2020-03-03 14:14:00 +0100

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ port = 3000;
 
 let data = require('./data');
 
-app.listen(port, () =>  {
+app.listen(process.env.PORT || port, function() =>  {
    console.log('le serveur fonctionne sur le port ${port}')
 })
 
@@ -18,12 +18,13 @@ app.use(
 
 app.options('*', cors());
 
-app.get("/codespromos", (req, res) => {
-    res.json(data);
+/*app.get("/codespromos", function(req, res) => {
+    res.json(data);*/
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'));
  });
 
-//app.get("/codespromos/:id", (req, res) => {
-app.get("/codespromos", (req, res) => {
+/*app.get("/codespromos/:id", function(req, res) => {
     const codepromoId = req.params.id;
     const codepromo = data.find(_codepromo => _codepromo.id === codepromoId);
  
@@ -32,4 +33,4 @@ app.get("/codespromos", (req, res) => {
     } else {
        res.json({ message: `Le code promo ${codepromoId} n'existe pas`})
     }
- });
+ });*/
