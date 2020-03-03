1a99fed70d07eebe4e3945c556d5555c38fdd7e7
alexandrecros1 <alexandre.cros1@epsi.fr>
2020-03-03 14:30:17 +0100

    correctif

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-var express = require('express'); 
-var app = express();
-var cors = require('cors');
+let express = require('express');
+let app = express();
+let cors = require('cors');
 const PORT = 3000;
 
 let data = require('./data');
 
-app.listen(process.env.PORT || 3000, () =>  {
-   console.log('le serveur fonctionne sur le port ${port}')
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log('le serveur fonctionne sur le port ${port}')
+});
 
 app.use(
-   cors({
-       credentials: true,
-       origin: true
-   })
+    cors({
+        credentials: true,
+        origin: true
+    })
 );
 
 app.options('*', cors());
 
 /*app.get("/codespromos", function(req, res) => {
     res.json(data);*/
-app.get('/', (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 /*app.get("/codespromos/:id", function(req, res) => {
     const codepromoId = req.params.id;
     const codepromo = data.find(_codepromo => _codepromo.id === codepromoId);
- 
+
     if (codepromo) {
        res.json(codepromo);
     } else {
