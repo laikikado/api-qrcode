2ca026446e85506cac8b4dacb425eefde305d99b
alexandrecros1 <alexandre.cros1@epsi.fr>
2020-03-06 14:05:27 +0100

    API TERMINEE

--- data.js
+++ data.js
@@ -1,20 +1,64 @@
 module.exports = [
-    {
-      "id": "1",
-      "title": "Carrefour",
-      "discount": "10%",
-      "endDate": "10/04/2020"
-    },
-    {
-      "id": "2",
-      "title": "Darty",
-      "discount": "20%",
-      "endDate": "01/01/2021"
-    },
-    {
-      "id": "3",
-      "title": "MacDo",
-      "discount": "15%",
-      "endDate": "29/06/2020"
-    }
+	{
+		"id": "1",
+		"title": "Supreme",
+		"discount": "-10%",
+		"endDate": "10/04/2020"
+	},
+	{
+		"id": "2",
+		"title": "Adidas",
+		"discount": "-20%",
+		"endDate": "01/01/2021"
+	},
+	{
+		"id": "3",
+		"title": "Vans",
+		"discount": "-15%",
+		"endDate": "29/06/2020"
+	},
+	{
+		"id": "4",
+		"title": "Nike",
+		"discount": "-5%",
+		"endDate": "10/02/2020"
+	},
+	{
+		"id": "5",
+		"title": "Volcom",
+		"discount": "-10%",
+		"endDate": "23/09/2020"
+	},
+	{
+		"id": "6",
+		"title": "Carhartt",
+		"discount": "-30%",
+		"endDate": "11/11/2020"
+	},
+	{
+		"id": "7",
+		"title": "New Era",
+		"discount": "-20%",
+		"endDate": "14/05/2020"
+	},
+	{
+		"id": "8",
+		"title": "Brixton",
+		"discount": "-10%",
+		"endDate": "28/08/2020"
+	},
+	{
+		"id": "8",
+		"title": "Uniqlo",
+		"discount": "-15%",
+		"endDate": "19/04/2020"
+	},
+	{
+		"id": "10",
+		"title": "Element",
+		"discount": "-10%",
+		"endDate": "01/03/2020"
+	}
   ]
+  
+  
