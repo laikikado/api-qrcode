a9d1938f85a61c8659c43dc063a7dedd78e03cb3
alexandrecros1 <alexandre.cros1@epsi.fr>
2020-03-06 13:59:17 +0100

    ouimec

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 let express = require('express');
 let app = express();
 let cors = require('cors');
+const port = 3000
 
 let data = require('./data');
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log('le serveur fonctionne sur le port ${port}')
+app.listen(process.env.PORT || port, function(){
+    console.log(`le serveur fonctionne sur le port ${port}`)
 });
 
 app.use(
@@ -18,15 +19,13 @@ app.use(
 app.options('*', cors());
 
 
-app.get("/codespromos", function(req, res) => {
+app.get("/codespromos", function(req, res) {
     res.json(data);
-/*app.get('/', function(req, res) {
-    res.send("hello world");
-});*/
+});
 
-app.get("/codespromos/:id", function(req, res) => {
+app.get("/codespromos/:id", function(req, res) {
     const codepromoId = req.params.id;
-    const codepromo = data.find(_codepromo => _codepromo.id === codepromoId);
+    const codepromo = data.find(_codepromo => _codepromo.id == codepromoId);
 
     if (codepromo) {
        res.json(codepromo);
