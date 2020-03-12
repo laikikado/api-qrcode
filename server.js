c2ba235d5a0897fe5b2cdc1c3f764a65955b0d18
alexandrecros1 <alexandre.cros1@epsi.fr>
2020-03-12 08:55:01 +0100

    fix api

--- server.js
+++ server.js
@@ -5,39 +5,54 @@ const port = 3000
 
 let data = require('./data');
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://root:root@cluster-jigqf.azure.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-	app.listen(process.env.PORT || port, function(){
-		console.log(`le serveur fonctionne sur le port ${port}`)
-	});
-  
-	app.use(
+app.listen(process.env.PORT || port, function(){
+    console.log(`le serveur fonctionne sur le port ${port}`)
+});
+
+async function main(){
+
+    const uri = "mongodb+srv://root:root@cluster/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        await listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
+
+app.use(
     cors({
         credentials: true,
         origin: true
-    }));
-	
-	app.options('*', cors());
-	
-	app.get("/codespromos", function(req, res) {
-    res.json(data);
-	});
-	app.get("/codespromos/:id", function(req, res) {
+    })
+);
+
+app.options('*', cors());
+
+app.get("/codespromos", function(req, res) {
+        res.json(data);
+    }
+);
+
+app.get("/codespromos/:id", function(req, res) {
     const codepromoId = req.params.id;
     const codepromo = data.find(_codepromo => _codepromo.id == codepromoId);
 
     if (codepromo) {
-       res.json(codepromo);
+        res.json(codepromo);
     } else {
-       res.json({ message: `Le code promo ${codepromoId} n'existe pas`})
+        res.json({ message: `Le code promo ${codepromoId} n'existe pas`})
     }
-	});
-  client.close();
-	
 });
-
-
-
